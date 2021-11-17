efbb9204a895a6c78adf516453e584e47f869999
ruslan.shukiurov <sukurovruslan68@gmail.com>
2021-11-17 10:47:08 +0200

    set httpOnly false

--- resolvers/usersResolver.js
+++ resolvers/usersResolver.js
@@ -29,8 +29,10 @@ module.exports = {
       const user = await loginUser({ email, password });
       res.setHeader(
         'Set-Cookie',
-        `${cookie.serialize('user', JSON.stringify({ token: user.token, id: user.id, nickname: user.nickname }))}; SameSite=None; Secure`,
-        { httpOnly: true, maxAge: 60 * 60 * 24 * 7 }
+        `${cookie.serialize(
+          'user', 
+          JSON.stringify({ token: user.token, id: user.id, nickname: user.nickname }))}; SameSite=None; Secure`,
+        { httpOnly: false, maxAge: 60 * 60 * 24 * 7 }
       );
 
       return { user };
@@ -51,8 +53,10 @@ module.exports = {
       const user = await registerUser({ nickname, firstName, lastName, email, password, googleId, facebookId });
       res.setHeader(
         'Set-Cookie',
-        cookie.serialize('user', JSON.stringify({ token: user.token, id: user.id, nickname: user.nickname })),
-        { httpOnly: true, maxAge: 60 * 60 * 24 * 7 }
+        `${cookie.serialize(
+          'user',
+          JSON.stringify({ token: user.token, id: user.id, nickname: user.nickname }))}; SameSite=None; Secure`,
+        { httpOnly: false, maxAge: 60 * 60 * 24 * 7 }
       );
 
       return { user: { ...user, id: user._id } };
